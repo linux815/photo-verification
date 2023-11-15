1a3992da8fdc9e895cd5d8bafd933ff8ca99f110
i.bazhenov <ivan.bazhenov@gmail.com>
2023-11-15 14:08:09 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
+cp .env.example .env
 docker-compose down
 docker-compose pull
 docker-compose up -d
 sleep 3
-cp .env.example .env
 sudo echo "127.0.0.1  photo.test" >> /etc/hosts
 docker exec -it photo_verification-php-fpm composer install
 docker exec -it photo_verification-php-fpm php yii migrate --interactive=0
